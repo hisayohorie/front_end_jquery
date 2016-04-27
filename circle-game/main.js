501fbf24ab845dc52f2c0b68f88db2e51ae1b05c
hisayohorie <hisayohorie@gmail.com>
2016-04-27 09:29:48 -0400

    ttt and circle game basic done

---
+++ circle-game/main.js
@@ -0,0 +1,53 @@
+function Circle() {
+  this.element = $('<div class="circle"></div>');
+  this.diameter = 50 + Math.ceil(Math.random() * 100);
+  this.position = {
+    x: this.newPosition(),
+    y: this.newPosition()
+  }
+
+  this.element.css({
+    width: this.diameter + 'px',
+    height: this.diameter + 'px',
+    top: this.position.y,
+    left: this.position.x
+  });
+}
+
+Circle.prototype.newPosition = function () {
+  return Math.random() * (750 - this.diameter)
+}
+
+Circle.prototype.move = function () {
+  var self = this;
+
+  this.element.animate({
+     top: this.newPosition(),
+     left: this.newPosition()
+   }, 10000, 'swing', function () {
+     self.move();
+   });
+}
+
+$(document).on('ready', function (){
+  var numberOfCircles = 10;
+
+  for(var i = 0; i < numberOfCircles; i++) {
+    var theCircle = new Circle();
+    $('#game').append(theCircle.element);
+    theCircle.move();
+  }
+
+  $('.circle').on('click', function(){
+      $(this).remove();
+      // var score = 0;
+      // $('.score').html(function(){
+      var score = parseInt($('span').html());
+      score++;
+      $('span').html(score);
+      // })
+
+    });
+
+
+});
---
+++ tic-tac-toe/ttt.js
@@ -0,0 +1,36 @@
+$(document).on('ready', function() {
+	var turn = 0;
+	var isX = true;
+	var winningCombos =[[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+	
+
+	$('td').on('click',function(){
+
+		function checkWinner(){
+			for(var i= 0; i < winningCombos.length; i++){
+				var combo = winningCombos[i];
+				
+				// combo will be [1,2,3]
+				if ( $('.X').hasClass(combo[0]) && $('.X').hasClass(combo[1]) && $('.X').hasClass(combo[2]) ) {
+					alert("X is the winner");
+				}else if($('.O').hasClass(combo[0]) && $('.O').hasClass(combo[1]) && $('.O').hasClass(combo[2])){
+					alert("O is the winner");
+				}
+				// compare each combo to a playerState array or use the DOM => s(".x")
+				// alert("we have a winner")
+			}
+		}
+		
+		if(turn % 2 === 0){
+			$(this).html("X").addClass("X");
+		}else{
+			$(this).html("O").addClass("O");
+		}
+		//check for winner
+		checkWinner();
+
+		$(this).off('click');
+		turn ++;
+
+	});
+});
